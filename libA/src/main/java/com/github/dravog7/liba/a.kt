c222db787bd1447edf907af0d38efa925953debf
dravog7 <dravog77@gmail.com>
2021-12-13 12:28:20 +0530

    init

---
+++ libA/src/main/java/com/github/dravog7/liba/a.kt
@@ -0,0 +1,9 @@
+package com.github.dravog7.liba
+
+import android.util.Log
+
+class a {
+    init {
+        Log.d("john", "in A")
+    }
+}
---
+++ libB/src/main/java/com/github/dravog7/libb/b.kt
@@ -0,0 +1,11 @@
+package com.github.dravog7.libb
+
+import android.util.Log
+import com.github.dravog7.liba.a
+
+class b {
+    init {
+        Log.d("john", "in B")
+        a()
+    }
+}
